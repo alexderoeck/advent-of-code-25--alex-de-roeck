d555419642da2c7d2329c70f3d273b315b24ca86
Alex de Roeck <alexcooperderoeck@gmail.com>
2026-08-23 13:27:31 +0100

    Attempt 2
    Attempt 2 of trying to complete the first 3 days of the Advent of code 2025 in 2hrs. See README for more information.

---
+++ day01part2attempt2/src/Main.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+void main() {
+
+    String testFilePath = "C:\\Users\\alexc\\IdeaProjects\\advent-of-code-25-adr\\.idea\\day01test.txt";
+    String filePath = "C:\\Users\\alexc\\IdeaProjects\\advent-of-code-25-adr\\.idea\\day01.txt";
+
+    int degrees = 50; // 50 DEGREES BY DEFAULT
+    int count = 0; // COUNT OF  ENDING ON ZERO
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+
+        String line;
+        while ((line = reader.readLine()) != null) { // FOR EACH LINE
+
+            //System.out.println(line);
+
+            int turnNum;
+            try { // CONVERTS THE STRING OF NUMBER(S) INTO AN INTEGER
+                turnNum = Integer.parseInt(line.substring(1));
+            }
+            catch (NumberFormatException e) {
+                System.out.println("ERROR - NumberFormatException");
+                turnNum = 0;
+            }
+
+            if ( line.charAt(0) == 'L' ) { // IF CODE STARTS WITH 'L'
+                for (int i = 1; i <= turnNum; i++) { // FOR THE TOTAL CLICKS
+                    degrees -= 1; // SUBTRACT 1
+                    if (degrees == -1) { // IF IT GOES TO -1
+                        degrees = 99; // SET TO 99
+                    }
+                    if ( degrees == 0 ) { // IF DEGREES END ON ZERO
+                        count += 1; // ADD ONE TO THE COUNT
+                    }
+                }
+            }
+            else if ( line.charAt(0) == 'R' ){ // IF THE CODE STARTS WITH 'R'
+                for (int i = 1; i <= turnNum; i++) { // FOR THE TOTAL CLICKS
+                    degrees += 1; // ADD 1
+                    if (degrees == 100) { // IF IT GOES TO 100
+                        degrees = 0; // SET TO 0
+                    }
+                    if ( degrees == 0 ) { // IF DEGREES END ON ZERO
+                        count += 1; // ADD ONE TO THE COUNT
+                    }
+                }
+            }
+            else { // ERROR CATCH
+                System.out.println("ERROR - CODE DOES NOT START WITH L or R");
+            }
+
+//            if ( degrees == 0 ) { // IF DEGREES END ON ZERO
+//                count += 1; // ADD ONE TO THE COUNT
+//            } // COMMENTED OUT AS REDUNDANT IN THIS PART
+
+        }
+    }
+
+    catch (FileNotFoundException e) { // CATCH FOR FILE NOT FOUND ERROR
+        System.out.println("ERROR - File not found");
+    }
+    catch (IOException e) { // CATCH FOR ERROR READING FILE
+        System.out.println("ERROR - Error reading file");
+    }
+
+    // OUTPUTS OF FINAL DEGREES AND COUNT
+    System.out.println("Final Degrees: " + degrees);
+    System.out.println("Final Count: " + count);
+}
---
+++ day02_P1/src/Main.java
@@ -0,0 +1,84 @@
+
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+void main() {
+
+    String testFilePath = "C:\\Users\\alexc\\IdeaProjects\\advent-of-code-25-adr\\.idea\\day02test.txt";
+    String filePath = "C:\\Users\\alexc\\IdeaProjects\\advent-of-code-25-adr\\.idea\\day02.txt";
+
+    long total = 0L; // TOTAL OF INVALID ID's
+
+    try (Scanner scan1 = new Scanner(new FileReader(filePath))) {
+        scan1.useDelimiter(",");
+
+        String range;
+        while ( scan1.hasNext() ) {
+            range = scan1.next();
+            System.out.println(range);
+
+            Scanner scan2 = new Scanner(range); // SCANS BETWEEN RANGE "-"
+            scan2.useDelimiter("-");
+
+            int start; // START OF RANGE ----------
+            try { // CONVERTS THE STRING OF NUMBER(S) INTO AN INTEGER
+                start = Integer.parseInt(scan2.next());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("ERROR - NumberFormatException");
+                start = 0;
+            }
+
+            int end; // END OF RANGE ----------
+            try { // CONVERTS THE STRING OF NUMBER(S) INTO AN INTEGER
+                end = Integer.parseInt(scan2.next());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("ERROR - NumberFormatException");
+                end = 0;
+            }
+
+            System.out.println("Start: " + start + " End: " + end);
+
+
+            // CHECKING THE RANGE FOR INVALID ID's
+            for (int i = start; i <= end; i++) { // FOR ALL NUMBERS IN RANGE
+
+                String temp = String.valueOf(i);
+                int len = temp.length(); // GET LENGTH OF STRING
+                if ( temp.startsWith("0") ){ //  IF IT LEADS WITH ZERO ITS INVALID
+                    total += i; // ADD TO TOTAL
+                    System.out.println(i + " Added to the total");
+                    System.out.println("Total is " + total);
+                }
+                else if ( (len % 2) == 0) { // IF EVEN CONTINUE, AS ODD MUST BE VALID
+
+                    int mid = (len / 2); // GET MIDDLE OF STRING
+                    String firstHalf = temp.substring(0, mid); // GET FIRST HALF
+                    String secondHalf = temp.substring(mid); // GET SECOND HALF
+                    boolean isInvalid = firstHalf.equals(secondHalf); // CHECK IF HALVES ARE SAME -> INVALID
+                    if (isInvalid) { // IF INVALID ID
+                        total += i; // ADD TO TOTAL
+                        System.out.println(i + " Added to the total");
+                        System.out.println("Total is " + total);
+                    }
+
+                }
+
+            }
+        }
+
+    }
+
+    catch (FileNotFoundException e) {
+        System.out.println("ERROR - FILE NOT FOUND");
+    }
+    catch (IOException e) {
+        System.out.println("ERROR - ERROR READING FILE");
+    }
+
+    // OUTPUTS OF FINAL TOTAL
+    System.out.println("Final Total: " + total);
+
+}
